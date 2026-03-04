a57365da9ea226a8afcd80698e5fbec239835c02
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-05 00:59:42 +0430

    Using the Expanded Widget to Create Flexible Layouts

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,14 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(child: Image.asset("images/dice1.png")),
+        Expanded(child: Image.asset("images/dice2.png")),
+      ],
+    );
   }
 }
