4fd7795b9b47f94b22bde47f6fc3026767f68cac
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-05 01:14:07 +0430

    How to Use Intention Actions

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,23 @@ class DicePage extends StatelessWidget {
   const DicePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(child: Image.asset("images/dice1.png")),
-        Expanded(child: Image.asset("images/dice2.png")),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("images/dice2.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
