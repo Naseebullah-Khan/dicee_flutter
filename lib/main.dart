bd636800e3f431d06dce0f664e13bc31aeb34f0c
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-05 14:32:11 +0430

    Stateful vs Stateless Widgets Whats the Difference

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 
-// Primitives Data Types -> String, Integer, Double and Boolean
-
-// we can change data of variable as long as we don't change its data type
-
-// var a; OR dynamic a; -> it has dynamic datatype mean you can assign any types od data
-
-// String a; OR var a = "string" -> only assign string
-// int a; OR var a = 1234 -> only assign integer
-// bool a; OR var a = false -> only assign boolean
-// double a; OR var a = 123.123 -> only assign double
-
-// Recommendation -> Avoid using var or dynamic datatypes unless it is necessary
-
 void main() {
   return runApp(
     MaterialApp(
@@ -25,17 +12,19 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
-  // DicePage({super.key});
 
-  // int leftDiceNumber = 2;
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    int leftDiceNumber = 1;
-    int rightDiceNumber = 1;
-
     return Center(
       child: Row(
         children: [
@@ -43,7 +32,11 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: GestureDetector(
-                onTap: () => {print("The left dice got tapped!")},
+                onTap: () => {
+                  setState(() {
+                    leftDiceNumber = 5;
+                  }),
+                },
                 child: Image.asset("images/dice$leftDiceNumber.png"),
               ),
             ),
